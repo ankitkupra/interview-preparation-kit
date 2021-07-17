07159ea877e6ef6d0474090b49c1485d75a71485
Ankit Kumar <ankitkupra@gmail.com>
2021-07-18 01:39:35 +0530

    Format File

--- interview-prepartion-kit/src/com/interviewpreparationkit/hackerrank/RepeatedString.java
+++ interview-prepartion-kit/src/com/interviewpreparationkit/hackerrank/RepeatedString.java
@@ -7,23 +7,23 @@ public class RepeatedString {
 		Scanner scan = new Scanner(System.in);
 		long n = scan.nextLong(); // number till sequence is to calculated
 		String str = scan.next(); // input string
-		
-		long result = repeatedString(str,n);
+
+		long result = repeatedString(str, n);
 		System.out.println(result);
 	}
 
 	private static long repeatedString(String s, long n) {
-		long num = n/s.length();
-        long rem = n%s.length();
-        long ans = 0;
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i)=='a') {
-                ans += num;
-                if (i < rem)
-                    ans++;
-            }
-        }
-    return ans;
+		long num = n / s.length();
+		long rem = n % s.length();
+		long ans = 0;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == 'a') {
+				ans += num;
+				if (i < rem)
+					ans++;
+			}
+		}
+		return ans;
 	}
 
 }
